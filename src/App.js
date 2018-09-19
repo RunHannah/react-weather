bd674a7b5213055f2517259734764d22bb0298c6
Hannah Lee <leexhannah@gmail.com>
2018-09-18 20:11:29 -0700

    Only render results if city and country are valid.

--- src/App.js
+++ src/App.js
@@ -27,16 +27,27 @@ class App extends Component {
         `&units=imperial`
     );
     const data = await api_call.json();
-    console.log("data", data);
 
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
-    });
+    if (city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter a valid value"
+      });
+    }
+    console.log("data", data);
   };
 
   render() {
@@ -44,7 +55,14 @@ class App extends Component {
       <div>
         <Title />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
