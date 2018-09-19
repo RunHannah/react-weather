c8aed31c07d8870aaf27afb8d4ebe71d7923f8f7
Hannah Lee <leexhannah@gmail.com>
2018-09-18 19:45:35 -0700

    Render API call.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,25 @@ import Form from "./components/form";
 import Weather from "./components/weather";
 
 class App extends Component {
+  getWeather = async e => {
+    e.preventDefault();
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=` +
+        process.env.REACT_APP_WEATHER_API_KEY
+    );
+    const data = await api_call.json();
+    console.log("data", data);
+  };
+
   render() {
     return (
       <div>
         <Title />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
