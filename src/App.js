413bb08cc0cd60b65026e16a10c22953431d51cf
Hannah Lee <leexhannah@gmail.com>
2018-09-18 19:53:28 -0700

    Identify state properties and api path.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import Form from "./components/form";
 import Weather from "./components/weather";
 
 class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+
   getWeather = async e => {
     e.preventDefault();
 
@@ -14,10 +23,20 @@ class App extends Component {
 
     const api_call = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=` +
-        process.env.REACT_APP_WEATHER_API_KEY
+        process.env.REACT_APP_WEATHER_API_KEY +
+        `&units=imperial`
     );
     const data = await api_call.json();
     console.log("data", data);
+
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
   };
 
   render() {
