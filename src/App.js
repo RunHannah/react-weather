a2bce984d057ebde8f5bcc6285ebeedec685a7e6
Hannah Lee <leexhannah@gmail.com>
2018-09-18 19:03:00 -0700

    Create template components.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import Title from "./components/title";
+import Form from "./components/form";
+import Weather from "./components/weather";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Title />
+        <Form />
+        <Weather />
+      </div>
+    );
+  }
+}
+
+export default App;
